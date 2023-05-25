f9700ba0ab5d1f757263aa7be2df3b0568a0801d
gly <reen.gly@qq.com>
2023-05-25 17:42:35 +0800

    20230525_l203_螺旋矩阵—easy

---
+++ src/link/l203.java
@@ -0,0 +1,39 @@
+package link;
+
+
+
+public class l203 {
+}
+
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode Head = new ListNode(0);
+        Head.next = head;
+        ListNode p = Head;
+        while (p.next != null){
+            if (p.next.val == val){
+                p.next = p.next.next;
+            }else {
+                p = p.next;
+            }
+        }
+        return Head.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
