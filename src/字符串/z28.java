d7ffb9d79c7e9a8fa81a3b4e5424563047b786d2
gly <reen.gly@qq.com>
2023-06-25 20:51:33 +0800

    20230625_z_Offer58_左旋字符——算法优化

---
+++ src/字符串/z28.java
@@ -0,0 +1,11 @@
+package 字符串;
+
+public class z28 {
+}
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        //KMP
+
+    }
+}
--- src/字符串/z_Offer58.java
+++ src/字符串/z_Offer58.java
@@ -3,16 +3,36 @@ package 字符串;
 public class z_Offer58 {
 }
 
-class Solution {
+//class Solution {
+//    public String reverseLeftWords(String s, int n) {
+//        int len = s.length();
+//        char[] chars = new char[len];
+//        for (int i = n; i < len; i++) {
+//            chars[i - n] = s.charAt(i);
+//        }
+//        for (int i = 0; i < n; i++) {
+//            chars[i + len - n] = s.charAt(i);
+//        }
+//        return String.valueOf(chars);
+//    }
+//}
+class Solution { //优化
     public String reverseLeftWords(String s, int n) {
         int len = s.length();
-        char[] chars = new char[len];
-        for (int i = n; i < len; i++) {
-            chars[i - n] = s.charAt(i);
+        StringBuilder stringBuilder = new StringBuilder(s);
+        reverse(stringBuilder, 0, n-1);
+        reverse(stringBuilder, n, len-1);
+        reverse(stringBuilder, 0, len-1);
+        return String.valueOf(stringBuilder);
+    }
+
+    public void reverse(StringBuilder s, int start, int end){
+        while(start<end){
+            char temp = s.charAt(start);
+            s.setCharAt(start, s.charAt(end));
+            s.setCharAt(end, temp);
+            start++;
+            end--;
         }
-        for (int i = 0; i < n; i++) {
-            chars[i + len - n] = s.charAt(i);
-        }
-        return String.valueOf(chars);
     }
 }
