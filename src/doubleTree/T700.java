d3b3efd1caae75e02cec4b6a2bec66c424dbd658
gly <reen.gly@qq.com>
2023-08-19 22:35:00 +0800

    20230819-搜索子树700-easy

---
+++ src/doubleTree/T700.java
@@ -0,0 +1,37 @@
+package doubleTree;
+
+import javax.swing.tree.TreeNode;
+
+public class T700 {
+}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+
+        if (root == null || root.val == val) {
+            return root;
+        }
+        if (root.val < val) {
+            return searchBST(root.right, val);
+        }
+
+        if (root.val >  val) {
+            return searchBST(root.left, val);
+        }
+        return null;
+    }
+}
